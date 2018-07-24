7e6fa4d7e6d8f16252723a3fc038ae4f5cbef993
Andrea Lizeth Quishpe Zapata <38335472+AndreaLizeth@users.noreply.github.com>
2018-07-24 09:04:43 -0500

    Add files via upload

---
+++ Interfaces/src/interfaces/Circulo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Circulo implements Figura {
+    private final double PI = 3.14;
+    double radio;
+    
+    
+    
+    
+    
+    
+    @Override
+    public double calcularArea(){
+    return 0.0;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+    return 0.0;    
+    }
+}
--- Interfaces/src/interfaces/Interfaces.java
+++ Interfaces/src/interfaces/Interfaces.java
@@ -16,6 +16,10 @@ public class Interfaces {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       // Figura f;
+        //f = cmb.setSelectedItem();
+        
     }
     
 }
